6a0b2fdd1ef58a402dec778f88e7180a4fec631c
Biganao <mfponte2006@gmail.com>
2023-01-17 16:49:33 -0300

    Feature: Projects.js
    Add: Loader and a condition if db dont have any projects

--- src/components/Pages/Projects.js
+++ src/components/Pages/Projects.js
@@ -4,13 +4,15 @@ import { useLocation } from "react-router-dom";
 import Message from "../layout/Message";
 import Container from "../layout/Container";
 import LinkBtn from "../layout/LinkBtn";
-
-import styles from "./Projects.module.css";
+import Loading from "../layout/Loading";
 
 import Card from "../Project/Card";
 
+import styles from "./Projects.module.css";
+
 function Projects() {
   const [projects, setProjects] = useState([]);
+  const [removeLoading, setRemoveLoading] = useState(false);
 
   const location = useLocation();
   let message = "";
@@ -20,18 +22,21 @@ function Projects() {
   }
 
   useEffect(() => {
-    fetch("http://localhost:5000/projects", {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
-      .then((resp) => resp.json())
-      .then((data) => {
-        console.log(data);
-        setProjects(data);
+    setTimeout(() => {
+      fetch("http://localhost:5000/projects", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
       })
-      .catch((err) => console.log(err));
+        .then((resp) => resp.json())
+        .then((data) => {
+          console.log(data);
+          setProjects(data);
+          setRemoveLoading(true);
+        })
+        .catch((err) => console.log(err));
+    }, 1000);
   }, []);
 
   return (
@@ -52,6 +57,10 @@ function Projects() {
               key={project.id}
             />
           ))}
+        {!removeLoading && <Loading />}
+        {removeLoading && projects.length === 0 && (
+          <p> Não há projetos cadastrados! </p>
+        )}
       </Container>
     </div>
   );
