788f2ec24c339e55c36e66d09ad186b28aa3097e
Biganao <mfponte2006@gmail.com>
2023-01-16 21:03:03 -0300

    Stylling: Projects page

--- src/components/Pages/Projects.js
+++ src/components/Pages/Projects.js
@@ -1,6 +1,10 @@
 import { useLocation } from "react-router-dom";
 
 import Message from "../layout/Message";
+import Container from "../layout/Container";
+import LinkBtn from "../layout/LinkBtn";
+
+import styles from "./Projects.module.css";
 
 function Projects() {
   const location = useLocation();
@@ -11,9 +15,15 @@ function Projects() {
   }
 
   return (
-    <div>
-      <h1> Meus Projetos </h1>
+    <div className={styles.project_container}>
+      <div className={styles.title_container}>
+        <h1> Meus Projetos </h1>
+        <LinkBtn to="/newproj" text="Criar Projeto" />
+      </div>
       {message && <Message type="success" msg={message} />}
+      <Container customClass="start">
+        <p> Projetos... </p>
+      </Container>
     </div>
   );
 }
