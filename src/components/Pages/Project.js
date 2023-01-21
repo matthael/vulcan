184216dba2661d65b47b8120ed8045fee02aa77b
Biganao <mfponte2006@gmail.com>
2023-01-21 18:49:50 -0300

    Feature: Services
    add: form to create services and send them to DB and added system to validate the costs of services
    
    Change: added services in DB

--- src/components/Pages/Project.js
+++ src/components/Pages/Project.js
@@ -1,3 +1,4 @@
+import { parse, v4 as uuidv4 } from "uuid";
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
@@ -6,6 +7,7 @@ import styles from "./Project.module.css";
 import Loading from "../layout/Loading";
 import Container from "../layout/Container";
 import Message from "../layout/Message";
+import ServiceForm from "../Service/ServiceForm";
 
 import Form from "../Project/Form";
 
@@ -61,6 +63,44 @@ function Project() {
       .catch((err) => console.log(err));
   }
 
+  function createService(project) {
+    setMessage("");
+    //last service
+    const lastService = project.services[project.services.length - 1];
+
+    lastService.id = uuidv4();
+
+    const lastServiceCost = lastService.cost;
+
+    const newCost = parseFloat(project.cost) + parseFloat(lastServiceCost);
+
+    //maximun value validation
+    if (newCost > parseFloat(project.budget)) {
+      setMessage("Orçamento ultrapassado, verifique o valor do serviço");
+      setType("error");
+      project.services.pop();
+      return false;
+    }
+
+    //add service cost to project total cost
+    project.cost = newCost;
+
+    //update project
+    fetch(`http://localhost:5000/projects/${project.id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(project),
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        //show services
+        console.log(data);
+      })
+      .catch((err) => console.log(err));
+  }
+
   function toggleProjectForm() {
     setShowProjectForm(!showProjectForm);
   }
@@ -108,7 +148,13 @@ function Project() {
                 {!showServiceForm ? "Adcionar Serviço" : "Fechar"}
               </button>
               <div className={styles.project_info}>
-                {showServiceForm && <div> Formulario do Serviço </div>}
+                {showServiceForm && (
+                  <ServiceForm
+                    handleSubmit={createService}
+                    btnText="Adcionar serviço"
+                    projectData={project}
+                  />
+                )}
               </div>
             </div>
             <h2> Serviços </h2>
